41efe25933846a246c4511490c54786f73e8f255
Brett Adams <brett.whynot@gmail.com>
2023-10-10 22:24:38 +0000

    Avoid using beta for now

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,3 @@
-/opt/splunk/bin/splunk cmd python3 -m pip install --upgrade -t src/main/resources/splunk/lib -r src/main/resources/splunk/lib/requirements.txt --no-dependencies
 yarn install
 yarn run build
 yarn run link:app
