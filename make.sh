4af0600ffe2a2fa3891c7dfa50cda1af22591a25
Brett Adams <brett.whynot@gmail.com>
2023-10-10 22:30:02 +0000

    fix make

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
 mv stage stage2
 node bin/build.js build
-mv stage hibp
+mv stage spl2modules
 mv stage2 stage
-chmod -R u=rwX,go= hibp/*
-chmod -R u-x+X hibp/*
-chmod -R u=rwx,go= hibp/bin/*
+chmod -R u=rwX,go= spl2modules/*
+chmod -R u-x+X spl2modules/*
+chmod -R u=rwx,go= spl2modules/bin/*
 rm $1
-tar -cpzf $1 --exclude=hibp/.* --exclude=hibp/local hibp
-rm -rf hibp
+tar -cpzf $1 --exclude=spl2modules/.* --exclude=spl2modules/local spl2modules
+rm -rf spl2modules
