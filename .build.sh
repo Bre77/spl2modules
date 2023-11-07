f82f7f392f8c4769f7d9e4463f2ebd2532fbf750
Brett Adams <brett.whynot@gmail.com>
2023-11-07 23:28:08 +0000

    Slim build

--- .build.sh
+++ .build.sh
@@ -5,12 +5,12 @@ yarn install --non-interactive
 yarn run build
 chmod -R u=rwX,go= stage/*
 chmod -R u-x+X stage/*
-chmod -R u=rwx,go= stage/bin/*
+#chmod -R u=rwx,go= stage/bin/*
 mv stage spl2modules
-python3 -m pip install --upgrade -t spl2modules/lib -r spl2modules/lib/requirements.txt --no-dependencies
-rm -rf spl2modules/lib/splunklib/__pycache__
-rm -rf spl2modules/lib/splunklib/searchcommands/__pycache__
-rm -rf spl2modules/lib/splunklib/modularinput/__pycache__
-rm -rf spl2modules/lib/*/__pycache__
+#python3 -m pip install --upgrade -t spl2modules/lib -r spl2modules/lib/requirements.txt --no-dependencies
+#rm -rf spl2modules/lib/splunklib/__pycache__
+#rm -rf spl2modules/lib/splunklib/searchcommands/__pycache__
+#rm -rf spl2modules/lib/splunklib/modularinput/__pycache__
+#rm -rf spl2modules/lib/*/__pycache__
 tar -cpzf $OUTPUT --exclude=spl2modules/.* --overwrite spl2modules 
 rm -rf spl2modules
